26043fff8784d1058e937606f5b08c3989ffa76b
nilesh05apr <nk8588338@gmail.com>
2022-12-03 20:28:18 +0530

    add:Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn urlshortner.urlshortner.wsgi.application --log-file - 
+web: python3 manage.py migrate && gunicorn urlshortner.wsgi --log-file - 
--- urlshortner/urlapp/urls.py
+++ urlshortner/urlapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import reroute,api,health
 
 urlpatterns = [
-    path('health/',health),
+    path('health',health),
     path('', api, name='api'),
     path('<str:pk>',reroute,name='reroute')
 ]
